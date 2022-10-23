0f45518f276694bcd14eeb688b7d2a02a9751efe
Fernando Aleixos <fernanaleixos0204@gmail.com>
2022-10-23 15:45:49 +0200

    Ejercicios repaso 4

---
+++ src/bases/Repaso_UD03/repaso3.3.js
@@ -0,0 +1,13 @@
+//window.onload = ;
+let entrada = prompt('Introduce los minutos:');
+entrada = parseInt(entrada);
+
+const hora = 0;
+const min = 0;
+const sec = 0;
+
+
+
+let crono = function(minutos) {
+    
+}
