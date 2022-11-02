f386df676696c965de8ff9ace67a93a944e2d551
Fernando Aleixos <fernanaleixos0204@gmail.com>
2022-11-02 19:46:38 +0100

    Ejercicio de repaso 6

---
+++ src/bases/Repaso_UD06/getv1.js
@@ -0,0 +1,29 @@
+const SERVER = 'https://dog.ceo/api/breeds/image/random';
+const imagen = document.getElementById('imagen');
+
+window.addEventListener('load', function() {
+    getPosts();
+});
+
+function getPosts() {
+    const peticion = new XMLHttpRequest();
+
+    peticion.open('GET', SERVER);
+    peticion.send();
+    
+    peticion.addEventListener('load', function() {
+        if(peticion.status === 200) {
+            const datos = JSON.parse(peticion.responseText);
+
+            console.log(datos);
+
+            const newPost = '';
+            newPost.innerHTML = `<img src="${datos}" alt="Hola">`;
+            imagen.appendChild(newPost);
+
+        } else {
+            console.error('Error' + peticion.status + ' (' +
+            peticion.statusText + ') en la petición');
+        }
+    });
+}
