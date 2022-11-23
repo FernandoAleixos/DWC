56b615a9a0897569d5b4abfab850b4fd96b5c86e
Fernando Aleixos <fernanaleixos0204@gmail.com>
2022-11-23 23:08:52 +0100

    Ejercicio de repaso bloque 1

--- src/bases/_REPASO_BLOQUE_1/act2-Peticiones-DOM/ejercicio1.2.js
+++ src/bases/_REPASO_BLOQUE_1/act2-Peticiones-DOM/ejercicio1.2.js
@@ -1,4 +1,27 @@
-window.addEventListener('load', function() {
-    const formulario = document.createElement('form');
-    
+const contenido = document.getElementById('contenido');
+const formulario = document.getElementById('formBusca');
+const idEntrada = document.querySelector('input');
+
+formulario.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    fetch(`https://fakestoreapi.com/products/${idEntrada.value}`)
+        .then(response => response.json())
+        .then(data => {
+            let divProd = document.createElement('div');
+            let titleProd = document.createElement('h4');
+            let imgProd = document.createElement('img');
+            let desProd = document.createElement('p');
+
+            titleProd.innerHTML = data.title;
+            imgProd.src = data.image;
+            desProd.innerHTML = data.description;
+
+            divProd.appendChild(titleProd);
+            divProd.appendChild(imgProd);
+            divProd.appendChild(desProd);
+
+            contenido.appendChild(divProd);
+        });
 });
